519d5fe8707a5d53b810666a2d567ffdfffdf216
Nataliia_Pleskanka <pleskanka.nataliia@gmail.com>
2024-08-15 17:32:41 +0200

    init commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
 import 'swiper/css';
-import anime from 'animejs/lib/anime.es';
+import { heroBg } from './js/hero.js';
+
 
 
 
