91476816d39f6fcb711954fb5b1d5b7753e80516
Ваше ім'я <constantin161089@gmail.com>
2024-08-15 18:24:36 +0300

    add bibl

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-import Swiper from 'swiper';
-import 'swiper/css';
-import anime from 'animejs/lib/anime.es';
+
 
 
 
