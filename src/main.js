467ed8a18b34b29d1aef34c42a54c3f02924596e
Ваше ім'я <constantin161089@gmail.com>
2024-08-16 09:43:06 +0300

    add input svg

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-import './js/hero.js';
 import './js/about.js';
+import './js/benefits.js';
+import './js/covers.js';
+import './js/faq.js';
+import './js/header.js';
+import './js/hero.js';
+import './js/projects.js';
+import './js/reviews.js';
+import './js/work.js';
