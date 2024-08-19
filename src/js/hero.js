b53a15dc42e2f3e57a0d982175cad479afdcf91c
Nataliia_Pleskanka <pleskanka.nataliia@gmail.com>
2024-08-19 22:15:39 +0200

    revision

--- src/js/hero.js
+++ src/js/hero.js
@@ -3,7 +3,7 @@ import anime from 'animejs';
 
 export const heroBg = document.addEventListener('DOMContentLoaded', () => {
   const animation = anime({
-    targets: '.glass-effect',
+    targets: '.background-effect',
     translateX: 52,
     duration: 1000,
     loop: true,
